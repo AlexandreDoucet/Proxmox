0125da9ede9fdd30f5cb8c040aa2423c5095d465
Alexandre Doucet <alexandre.doucet6@gmail.com>
2023-12-29 22:43:52 -0400

    Commiting initial set-up file. Setup.sh can be used to configure Proxmox for GPU passthroguh. vmsetup.sh can be used to partially configure en existing VM to allow GPU passthrough

---
+++ setup.sh
@@ -0,0 +1,80 @@
+
+
+#Changes command for Grub 
+
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet intel_iommu=on iommu=pt"/g' /etc/default/grub
+
+update-grub
+
+
+# Adds additionnal modules.
+sed -i '/vfio.*/d' /etc/modules
+
+echo "vfio">>/etc/modules
+echo "vfio_iommu_type1">>/etc/modules
+echo "vfio_pci">>/etc/modules
+echo "vfio_virqfd">>/etc/modules
+
+
+
+# No idea
+sed -i '/options vfio_iommu_type1 allow_unsafe_interrupts=1/d' /etc/modprobe.d/iommu_unsafe_interrupts.conf
+sed -i '/options kvm ignore_msrs=1/d' /etc/modprobe.d/kvm.conf
+
+
+echo "options vfio_iommu_type1 allow_unsafe_interrupts=1" > /etc/modprobe.d/iommu_unsafe_interrupts.conf
+echo "options kvm ignore_msrs=1" > /etc/modprobe.d/kvm.conf
+
+
+#Adds the usuall GPU drivers to the blacklist in order to prevent proxmox from using the GPU for the console. If you have an integrated card, you should be able to plug a monitor in order to access the console.
+
+sed -i '/blacklist radeon/d' /etc/modprobe.d/blacklist.conf
+sed -i '/blacklist nouveau/d' /etc/modprobe.d/blacklist.conf
+sed -i '/blacklist nvidia/d' /etc/modprobe.d/blacklist.conf
+
+
+echo "blacklist radeon" >> /etc/modprobe.d/blacklist.conf
+echo "blacklist nouveau" >> /etc/modprobe.d/blacklist.conf
+echo "blacklist nvidia" >> /etc/modprobe.d/blacklist.conf
+
+
+
+# GPU passthrough
+# This section will ask the user which graphic device should be passed through.
+
+results=($(lspci -v | grep VGA | sed 's/ /_/g'))
+
+#results=($(lspci -v | grep VGA | grep -o '[0-9][0-9]:[0-9][0-9].[0-9]'))
+
+select choice in "${results[@]}"; do
+	if [ "$choice" ]; then
+		echo "You selected : $choice"
+		result=$(echo $choice | grep -o '[0-9][0-9]:[0-9][0-9].')
+		break
+	else
+		echo "invalid"
+	fi
+done
+
+
+ids=($(lspci -v | grep -o $result[0-9] | sed 's/ /_/g' ))
+
+concatID=""
+
+for id in "${ids[@]}"; do
+## Concat the results of the line bellow into the format []:[],[]:[] which can in used in the next command.
+	res=$(lspci -n -s $id | grep -o [0-9,a-z][0-9,a-z][0-9,a-z][0-9,a-z]:[0-9,a-z][0-9,a-z][0-9,a-z][0-9,a-z])
+	concatID+=$res","
+done
+concatID=${concatID: : -1}
+echo $concatID
+
+sed -i '/options vfio-pci ids=.* disable_vga=.*/d' /etc/modprobe.d/vfio.conf
+
+echo "options vfio-pci ids=$concatID disable_vga=1"> /etc/modprobe.d/vfio.conf
+
+
+# Updates the changes, computer should now restart and the selected GPU will no longer be available to proxmox. 
+update-initramfs -u
+reset
+
---
+++ vmsetup.sh
@@ -0,0 +1,36 @@
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+filePath="/etc/pve/qemu-server/"
+
+
+echo "This operation will now change a few configurations on a VM to allow GPU passthrough."
+echo "The VM must already be created before running this script and may not work otherwise."
+
+printf "\n~~~~~~~~~~~~~\nHere are the current available Vms\n"
+ls $filePath
+printf "\n~~~~~~~~~~~~\n"
+
+path=""
+while :
+do
+	read -p 'VM id : ' id
+	path="$filePath$id.conf"
+
+	if [ -f "$path" ]; then
+		break
+	fi
+
+	echo -e "${RED}A configuration fire for this if does not exist. Did you create the VM ?${NC}"
+	printf "\n\n"
+done
+
+echo "you have selected $id. File located at $path has been modified."
+
+echo "machine: q35">> $path
+echo "cpu: host,hidden=1,flags=+pcid">> $path
+echo "args: -cpu 'host,+kvm_pv_unhalt,+kvm_pv_eoi,hv_vendor_id=NV43FIX,kvm=off'">> $path
+
+
+
+
